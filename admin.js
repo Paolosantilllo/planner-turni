1b5ca0682fcacce8ea3df7b2a09d2b6da5ee05ce
Paolo Santillo <paolosantillo@yahoo.it>
2026-07-01 17:32:01 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,24 @@
 
+function goHome() {
+  window.location.href = "index.html";
+}
+
+function openEmployees() {
+  document.getElementById("adminContent").innerHTML =
+    "<h2>👥 Gestione Dipendenti</h2>";
+}
+
+function openStats() {
+  document.getElementById("adminContent").innerHTML =
+    "<h2>📊 Statistiche reperibilità</h2>";
+}
+
+function exportPDF() {
+  document.getElementById("adminContent").innerHTML =
+    "<h2>📄 Export PDF</h2>";
+}
+
+function openSettings() {
+  document.getElementById("adminContent").innerHTML =
+    "<h2>⚙️ Impostazioni</h2>";
+}
