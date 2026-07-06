55d939ce4de9133bd982c292a70f19447ad48643
Paolo Santillo <paolosantillo@yahoo.it>
2026-07-06 15:14:33 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,46 @@
 
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.createEmployee = functions.https.onCall(async (data, context) => {
+
+  try {
+
+    // 🔐 SOLO ADMIN
+    if (!context.auth || context.auth.token.role !== "ADMIN") {
+      throw new functions.https.HttpsError("permission-denied", "Not allowed");
+    }
+
+    const { email, password, employee, role } = data;
+
+    if (!email || !password) {
+      throw new functions.https.HttpsError("invalid-argument", "Missing fields");
+    }
+
+    // 1. CREA UTENTE AUTH
+    const userRecord = await admin.auth().createUser({
+      email,
+      password
+    });
+
+    // 2. CREA FIRESTORE USER
+    await admin.firestore().collection("users").doc(userRecord.uid).set({
+      email,
+      employee,
+      role: role || "USER",
+      active: true,
+      fcmTokens: []
+    });
+
+    return {
+      success: true,
+      uid: userRecord.uid
+    };
+
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.message);
+  }
+
+});
