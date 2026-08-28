a1dda9141af68e75bb860fc11abd0da4f7a3043d
archivio boll <paolosantillo@yahoo.it>
2026-08-28 06:33:07 +0200

    Importa updatePassword per cambio password

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ import {
   createUserWithEmailAndPassword,
   updateEmail,
   verifyBeforeUpdateEmail,
+  updatePassword,
   signOut
 } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
